d1ac3e79143607bf9843c14d69fffb003a05e7d7
risaaak <104627649+risaaak@users.noreply.github.com>
2022-10-26 11:20:20 +0800

    Add playRound function

--- script.js
+++ script.js
@@ -6,3 +6,23 @@ function getComputerChoice() {
     return choice;
 }
 
+function playRound(playerSelection , computerSelection) {
+    if(playerSelection == 'rock' && computerSelection == 'scissor')
+        return "You win! rock beats scissor ";
+    else if (playerSelection == 'paper' && computerSelection == 'rock')
+        return "You win! paper beats rock";
+    else if (playerSelection == 'scissor' && computerSelection == 'paper')
+        return "You win! scissor beats paper";
+    else if (playerSelection == 'rock' && computerSelection == 'paper')
+        return "You loose! paper beats rock";
+    else if (playerSelection == 'paper' && computerSelection == 'scissor')
+        return "You loose! scissor beats paper"
+    else if(playerSelection == 'scissor' && computerSelection == 'rock')
+        return "You loose! rock beats scissor";
+    else if(playerSelection == computerSelection)
+        return "Its a tie!";
+}
+
+computerSelection = getComputerChoice();
+playerSelection = 'rock';
+
